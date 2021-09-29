46aee054150df573c3c90a69b6103fa199f85c70
Mark A. Matienzo <matienzo@stanford.edu>
2021-09-29 11:03:22 -0700

    Fix unlabeled anchor links

--- assets/js/template-dom-scripts.js
+++ assets/js/template-dom-scripts.js
@@ -70,6 +70,8 @@
 
         var link = document.createElement('a');
         link.setAttribute('href', '#' + id);
+        link.setAttribute('aria-label', 'link to this heading');
+        link.setAttribute('aria-describedby', id);
         link.innerHTML = '<svg aria-hidden="true" class="link-icon" viewBox="0 0 50 50" focusable="false"> <use xlink:href="#link"></use> </svg>';
 
         container.appendChild(link);
